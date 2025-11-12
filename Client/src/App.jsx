a8f12d88c2fd3cb6a010f4d92770c9f5221b3357
Abhijeet Kumar <abhijeetkumar@Abhijeets-MacBook-Pro-3.local>
2025-11-13 00:42:51 +0530

    initialised frontend of the Nudgeusing react javascript and tailwind

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-red-500 text-white text-3xl font-bold flex justify-center items-center h-screen">
+        HII from Keshav ,   
+         How are you? Keshav is asking 😋😋 !!!
+    </div>
+  )
+}
+
+export default App
